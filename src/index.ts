6b97ddccb05480afd236e69205f11940695473bb
Robert Damaceno <robertdamacenobob@gmail.com>
2020-10-29 13:24:56 -0300

    config to production

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: false
 }))
 
 app.listen(process.env.PORT || 8081, () => console.log('http://localhost:8081/graphql'))
