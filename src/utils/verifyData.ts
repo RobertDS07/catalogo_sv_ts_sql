200b6f28e56064ac0bf2c139b1ddcdb96f569205
Robert Damaceno <robertdamacenobob@gmail.com>
2020-11-03 20:13:50 -0300

    :star: eslint

--- src/utils/verifyData.ts
+++ src/utils/verifyData.ts
@@ -1,9 +1,11 @@
 const verifyData = <T>(data: T) => {
     for (const property in data) {
-        const toVerify = String(data[property]).trim()
+        if (Object.prototype.hasOwnProperty.call(data, property)) {
+            const toVerify = String(data[property]).trim()
 
-        if (toVerify === undefined || toVerify === '' || toVerify === null)
-            throw new Error('Preencha todos os campos obrigatórios!')
+            if (toVerify === undefined || toVerify === '' || toVerify === null)
+                throw new Error('Preencha todos os campos obrigatórios!')
+        }
     }
 }
 
